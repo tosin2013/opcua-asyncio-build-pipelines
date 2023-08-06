647fc2f85ad43dbe12e4b5773d19eece6b746667
Tosin Akinosho <takinosh@redhat.com>
2023-08-06 18:44:52 -0400

    passenger-service.py

--- mock-ship/passenger-service/passenger-service.py
+++ mock-ship/passenger-service/passenger-service.py
@@ -1,29 +1,34 @@
 import asyncio
 import logging
-from asyncua import Server, ua
+from asyncua import Server
 from random import randint, uniform, choice
 
 # Setup logging configuration
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+_logger = logging.getLogger("asyncua")
 
 class CruiseServiceSimulator:
     def __init__(self):
         self.server = Server()
-        self.server.set_endpoint("opc.tcp://0.0.0.0:4840/freeopcua/server/")
-        self.namespace_index = 0  # Will be set during async initialization
-        self.root = None  # Will be set during async initialization
-        logging.info("CruiseServiceSimulator initialized.")
-
-    async def initialize_services(self):
-        self.namespace_index = await self.server.register_namespace("CruiseServices")
-        self.root = await self.server.nodes.objects.add_object(self.namespace_index, "CruiseServices")
+        self.endpoint = "opc.tcp://0.0.0.0:4840/freeopcua/server/"
         
+    async def initialize_services(self):
+        await self.server.init()
+        self.server.set_endpoint(self.endpoint)
+        
+        # setup our own namespace
+        uri = "http://examples.freeopcua.github.io"
+        self.namespace_index = await self.server.register_namespace(uri)
+
+        # add engine room object to the server
+        self.root = await self.server.nodes.objects.add_object(self.namespace_index, "EngineRoom")
+
         # Restaurant names and initial settings
         self.restaurant_names = ["Le Gourmet", "Ocean's Delight", "Nautical Bites", "Deck Dining", "Mariner's Munchies"]
         self.restaurant_name = choice(self.restaurant_names)
         self.restaurant_name_node = await self.root.add_variable(self.namespace_index, "RestaurantName", self.restaurant_name)
         self.restaurant_occupancy = await self.root.add_variable(self.namespace_index, "RestaurantOccupancy", 50)
-        
+
         # Pool locations and initial settings
         self.pool_locations = ["Front Deck", "Rear Deck", "Mid-Ship", "Starboard Side", "Port Side"]
         self.pool_location = choice(self.pool_locations)
@@ -35,30 +40,27 @@ class CruiseServiceSimulator:
         await self.pool_location_node.set_writable()
         await self.pool_temperature.set_writable()
 
-        logging.info(f"Services initialized with Restaurant: {self.restaurant_name} and Pool Location: {self.pool_location}")
+        _logger.info(f"Services initialized with Restaurant: {self.restaurant_name} and Pool Location: {self.pool_location}")
 
     async def simulate_services(self):
-        while True:
-            # Simulate random changes in restaurant occupancy and pool temperature
-            restaurant_change = randint(-5, 5)
-            pool_temp_change = uniform(-0.5, 0.5)
+        # Simulate random changes in restaurant occupancy and pool temperature
+        restaurant_change = randint(-5, 5)
+        
+        new_occupancy = await self.restaurant_occupancy.read_value() + restaurant_change
+        new_occupancy = max(0, min(100, new_occupancy))
+        await self.restaurant_occupancy.set_value(new_occupancy)
 
-            new_occupancy = self.restaurant_occupancy.get_value() + restaurant_change
-            new_occupancy = max(0, min(100, new_occupancy))
-            await self.restaurant_occupancy.set_value(new_occupancy)
+        pool_temp_change = uniform(-0.5, 0.5)
+        new_temperature = await self.pool_temperature.read_value() + pool_temp_change
+        await self.pool_temperature.set_value(new_temperature)
 
-            new_temperature = self.pool_temperature.get_value() + pool_temp_change
-            await self.pool_temperature.set_value(new_temperature)
-
-            logging.info(f"Updated Restaurant Occupancy: {new_occupancy}% and Pool Temperature: {new_temperature:.2f}°C")
-            await asyncio.sleep(5)
+        _logger.info(f"Updated Restaurant Occupancy: {new_occupancy}% and Pool Temperature: {new_temperature:.2f}°C")
+        await asyncio.sleep(5)
 
     async def run(self):
         await self.initialize_services()
-        logging.info("Starting OPC UA server.")
-
+        
         async with self.server:
-            # Continuously update services while the server is running
             while True:
                 await self.simulate_services()
                 await asyncio.sleep(1)
